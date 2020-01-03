6128361f023d158bd361f709987dfb76570cf7dd
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2020-01-03 12:33:49 +0700

    add confirm payment

--- app/Http/Controllers/api/v1/PaymentController.php
+++ app/Http/Controllers/api/v1/PaymentController.php
@@ -5,17 +5,44 @@ namespace App\Http\Controllers\api\v1;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Order;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class PaymentController extends Controller
 {
     public function confirm(Request $request)
     {
-        $idPesanan = $request->id_pesanan;
-        $order = Order::find($idPesanan);
-        $transfer = $order->transfer()->first();
+        DB::beginTransaction();
+        try {
+            $idPesanan = $request->id_pesanan;
+            $order = Order::find($idPesanan);
+            $transfer = $order->transfer()->first();
 
-        $namaPengirim = $request->nama_pengirim;
-        $bankPengirim = $request->bank_pengirim;
-        $buktiPembayaran = $request->bukti_pembayaran;
+            $buktiPembayaran = $request->bukti_pembayaran;
+            $buktiPembayaranUrl = $buktiPembayaran->store('proof_of_payment');
+
+            $transfer->Nama_Pemegang_Rekening = $request->nama_pengirim;
+            $transfer->Nama_Bank_Pengirim = $request->bank_pengirim;
+            $transfer->Bukti_Transfer = $buktiPembayaranUrl;
+            $transfer->Tgl_Transfer = Carbon::now();
+
+            $order->Status_Pesanan = 'menunggu_verifikasi';
+            
+            $order->save();
+            $transfer->save();
+
+            DB::commit();
+            
+            return response()->json([
+                'success' => TRUE,
+                'message' => 'Bukti Pembayaran Berhasil Upload!',
+            ], 201);
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json([
+                'success' => FALSE,
+                'message' => $e,
+            ], 500);
+        }
     }
 }
