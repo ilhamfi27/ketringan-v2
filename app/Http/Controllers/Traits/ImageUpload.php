3432a50ce4bb31631d8fe15d629f77d9733eeb7a
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2020-01-04 07:31:05 +0700

    getting image upload for staging ready

--- app/Http/Controllers/Traits/ImageUpload.php
+++ app/Http/Controllers/Traits/ImageUpload.php
@@ -1,11 +1,18 @@
 <?php
 
 namespace App\Http\Controllers\Traits;
+use JD\Cloudder\Facades\Cloudder;
 
 trait ImageUpload
 {
     public function storePaymentProof($file)
     {
+        if(env('APP_ENV') != 'local'){
+            Cloudder::upload($file, null, [
+                'folder' => 'ketringan/payment_confirmation/'
+            ]);
+            return Cloudder::getResult()['url']; // to direct get image url from cloudinary
+        }
         return $file->store('proof_of_payment');
     }
 }
